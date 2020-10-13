570be71fc443b1b8f00b69e2e998dec67ff6de12
jikoso2 <jaroslaw-czerniak@o2.pl>
2020-10-14 01:11:12 +0200

    Uwzględnienie dat

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,22 @@ import com.itextpdf.text.pdf.PdfWriter;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
 import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, DocumentException {
 
-        PDFGenerator gen = new PDFGenerator();
+        PDFGenerator gen = new PDFGenerator(123,"Jarosław","Czerniak");
         gen.finalGenerator();
 
+        Date nowDate = new Date();
+        System.out.println("Domyslny:" + nowDate);
+        SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd" );
+        System.out.println(sdf1.format(nowDate));
     }
 
 
--- src/PDFGenerator.java
+++ src/PDFGenerator.java
@@ -7,18 +7,28 @@ import com.itextpdf.text.pdf.PdfWriter;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
 import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.stream.Stream;
 
 public class PDFGenerator {
+    private int factureNumber;
+    private String name;
+    private String secondname;
 
-            public void finalGenerator () throws FileNotFoundException, DocumentException {
+    public PDFGenerator(int factureNumber, String name, String secondname) {
+        this.factureNumber = factureNumber;
+        this.name = name;
+        this.secondname = secondname;
+    }
+
+    public void finalGenerator () throws FileNotFoundException, DocumentException {
                 var doc = new Document();
-                int number = 123;
-                PdfWriter.getInstance(doc, new FileOutputStream("Faktura numer " + number + ".pdf"));
+                PdfWriter.getInstance(doc, new FileOutputStream("Faktura " + factureNumber +"-"+ dateNumber()+" "+name+" "+secondname +".pdf"));
                 doc.open();
-                addFactureTitle(doc,number);
+                addFactureTitle(doc,factureNumber);
                 addAnything(doc);
                 doc.close();
             }
@@ -31,14 +41,29 @@ public class PDFGenerator {
                 document.add(new Paragraph("Title of the document"));
                 Paragraph paragraph = new Paragraph();
                 addEmptyLine(paragraph,2);
+                paragraph.add("Faktura wystawiona dnia: ");
+                addDate(paragraph);
                 document.add(paragraph);
                 document.add (new Paragraph("Sprawdzenie"));
+
     }
 
     private void addEmptyLine(Paragraph paragraph,int number) {
         for (int i = 0; i < number; i++) {
             paragraph.add(new Paragraph("  "));
         }
+    }
+
+    private void addDate(Paragraph paragraph) {
+        Date nowDate = new Date();
+        SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd" );
+        paragraph.add(new Paragraph(sdf1.format(nowDate)));
+    }
+
+    private String dateNumber() {
+        Date nowDate = new Date();
+        SimpleDateFormat sdf1 = new SimpleDateFormat("yy" );
+        return sdf1.format(nowDate);
     }
 
 }
